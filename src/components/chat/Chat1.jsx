9e3681c7f5dbca2f9fba1e9bc76e70378c4115e4
keitake123 <keitaketsuna@gmail.com>
2024-09-06 18:13:04 -0400

    f

--- src/components/chat/Chat1.jsx
+++ src/components/chat/Chat1.jsx
@@ -2,8 +2,36 @@ import "./chat.css";
 
 const Chat = () => {
     return (
-        <div className ='chat'>Chat</div>
-    )
+        <div className='chat'>
+            <div className='top'>
+                <div className='user'>
+                    <img src="./avatar.png" alt="Avatar" />
+                    <div className="texts">
+                        <span>Josh J</span>
+                        <p>Hi, my name is Josh</p>
+                    </div>
+                </div>
+                <div className='icons'>
+                    <img src="./phone.png" alt="Phone" />
+                    <img src="./video.png" alt="Video Call" />
+                    <img src="./info.png" alt="Info" />
+                </div>
+            </div>
+            <div className='center'></div>
+            <div className='bottom'>
+                <div className='icons'>
+                    <img src="./img.png" alt="Image" />
+                    <img src="./camera.png" alt="Camera" />
+                    <img src="./mic.png" alt="Mic" />
+                </div>    
+                <input type="text" placeholder="Type a message..." /> {/* Corrected placeholder */}
+                <div className='emoji'>
+                    <img src="./emoji.png" alt="Emoji" />  
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
+        </div>
+    );
 }
 
-export default Chat
+export default Chat;
