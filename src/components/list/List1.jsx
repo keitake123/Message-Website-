b77995c44a2360e2ad529d1f9fa868cd500451b0
keitake123 <keitaketsuna@gmail.com>
2024-09-06 16:36:45 -0400

    b

--- src/components/list/List1.jsx
+++ src/components/list/List1.jsx
@@ -1,8 +1,13 @@
 import "./list.css";
+import Userinfo from "./userinfo/Userinfo1"
+import Chatlist from "./chatlist/Chatlist1"
 
 const List = () => {
     return (
-        <div className ='list'>List</div>
+        <div className ='list'>
+        <Userinfo/>
+        <Chatlist/>
+        </div>
     )
 }
 
--- src/components/list/userinfo/Userinfo1.jsx
+++ src/components/list/userinfo/Userinfo1.jsx
@@ -2,7 +2,17 @@ import "./userinfo.css";
 
 const Userinfo = () => {
     return (
-        <div className ='userinfo'>Unserinfo</div>
+        <div className ='userinfo'>
+        <div className ="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Keit</h2>
+        </div>
+        <div className ='icons'>
+            <img src="./more.png" alt="" />
+            <img src="./video.png" alt="" />
+            <img src="./edit.png" alt="" />
+        </div>
+       </div> 
     )
 }
 
