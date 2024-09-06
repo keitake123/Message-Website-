6dd59aa385eb9316c6f661485b05c040947470c6
keitake123 <keitaketsuna@gmail.com>
2024-09-06 16:47:21 -0400

    c

--- src/components/list/chatlist/Chatlist1.jsx
+++ src/components/list/chatlist/Chatlist1.jsx
@@ -2,8 +2,16 @@ import "./chatlist.css";
 
 const Chatlist = () => {
     return (
-        <div className ='chatlist'>Chatlist</div>
-    )
+        <div className='chatlist'>
+            <div className="search">
+                <div className="searchbar">
+                    <img src="./search.png" alt="" /> 
+                    <input type="text" placeholder="Search" />
+                </div>
+                <img src="./plus.png" alt="" />
+            </div>
+        </div>
+    );
 }
 
-export default Chatlist
+export default Chatlist;
