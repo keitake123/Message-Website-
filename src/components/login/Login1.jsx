de5252bb9a8fd17f760a3d2f1e4d5dbc5329cc00
keitake123 <keitaketsuna@gmail.com>
2024-09-06 22:15:00 -0400

    i

--- src/components/login/Login1.jsx
+++ src/components/login/Login1.jsx
@@ -1,6 +1,13 @@
 import "./login.css";
 
 const Login = () => {
+    const [avatar, setAvatar] = useState({
+      file: null,
+      url: "",
+    });
+
+    const handleAvatar = e = 
+
        return <div className="login">
           <div className="item">
             <h2>Welcome back,</h2>
@@ -14,7 +21,8 @@ const Login = () => {
             <div className="Item">
             <h2>Create an Account</h2>
             <form>
-            <input type="file" id="file"/>
+            <label htmlFor ="file">Upload an image</label>
+            <input type="file" id="file" style={{display:"none"}} onchange={handleAvatar}/>
             <input type="text" placeholder="Username" name="username" />
             <input type="password" placeholder="Password" name="password" />
             <button>Sign In </button>
