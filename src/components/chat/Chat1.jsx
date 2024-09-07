8563e95569f3c6e3bebfa6675d133a8b7a2cca9d
keitake123 <keitaketsuna@gmail.com>
2024-09-06 20:03:17 -0400

    g

--- src/components/chat/Chat1.jsx
+++ src/components/chat/Chat1.jsx
@@ -1,6 +1,18 @@
+import {useState} from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+
 
 const Chat = () => {
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const handleEmoji = (e) => {
+        setText((prev) => prev + e.emoji);
+        setOpen(false);
+      };
+   
+
     return (
         <div className='chat'>
             <div className='top'>
@@ -17,16 +29,92 @@ const Chat = () => {
                     <img src="./info.png" alt="Info" />
                 </div>
             </div>
-            <div className='center'></div>
+            <div className='center'>
+            <div className='message'>
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message own'>
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message'>
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message own'>
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message'>
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message own'>
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message'>
+            <img src="./avatar.png" alt="" />
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            <div className='message own'>
+            <div className='texts'>
+            <p>
+            afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa nin afefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa ninafefav feffanend  fniqninw fnqwnfa nin
+            </p>
+            <span>1 min ago</span>
+            </div>
+            </div>
+            </div>
+
             <div className='bottom'>
                 <div className='icons'>
                     <img src="./img.png" alt="Image" />
                     <img src="./camera.png" alt="Camera" />
                     <img src="./mic.png" alt="Mic" />
                 </div>    
-                <input type="text" placeholder="Type a message..." /> {/* Corrected placeholder */}
+                <input 
+                type="text" 
+                placeholder="Type a message..." 
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                />
                 <div className='emoji'>
-                    <img src="./emoji.png" alt="Emoji" />  
+                    <img src="./emoji.png" alt=""onClick={() => setOpen((prev) => !prev)} />
+                    <EmojiPicker open = {open} onEmojiClick={handleEmoji}/>  
                 </div>
                 <button className="sendButton">Send</button>
             </div>
